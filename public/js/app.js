7e63366d6a80c16828dcd7006343be1cda3873fe
Andy Braga <ab@luckypike.com>
2017-02-27 12:30:34 +0300

    Бесконечная прокрутка и подсветска активной фотки

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,8 @@ $(function() {
   if(_bx.length > 0) {
     $('.bxslider').bxSlider({
       pagerCustom: '#bx-pager',
-      hideControlOnEnd: true,
-      infiniteLoop: false,
+      // hideControlOnEnd: true,
+      // infiniteLoop: false,
       nextText: '<svg viewBox="0 0 60 60"><polyline points="25,16 35,30 25,44 "/></svg>',
       prevText: '<svg viewBox="0 0 60 60"><polyline points="35,16 25,30 35,44 "/></svg>',
     });
