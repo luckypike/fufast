f611565cb3901116d03b63ba3ab7ca7c28a62c16
Andy Braga <ab@luckypike.com>
2019-09-30 11:51:02 +0300

    Добавлен linter

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'import/no-webpack-loader-syntax': 'off'
+  }
+}
