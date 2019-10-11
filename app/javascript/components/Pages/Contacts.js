74a06fd7cdcd80fd9d5924f8206247254e464694
Maxim <max.spiridonov@gmail.com>
2019-10-11 15:32:31 +0300

    Доделана страница "контакты"

--- app/javascript/components/Pages/Contacts.js
+++ app/javascript/components/Pages/Contacts.js
@@ -1,18 +1,62 @@
 import React from 'react'
+import { YMaps, Map, Placemark } from 'react-yandex-maps'
 
 import page from '../Page.module.css'
+import styles from './Contacts.module.css'
 
 export default function Contacts () {
   return (
     <div className={page.root}>
+      <div className={styles.image} />
 
       <div className={page.text}>
         <h1>
-          О компании
+          Контакты
         </h1>
 
         <p>
-          Контакты
+          ООО «Поволжье-спецодежда» (торговая марка «Фуфайка»)
+        </p>
+        <h2>Телефон</h2>
+        <p>
+          + 7 910 793 7733
+        </p>
+        <h2>
+          Время работы
+        </h2>
+        <p>
+          ПН-ПТ: с 9:00 до 18:00
+        </p>
+        <h2>
+          Адрес
+        </h2>
+        <p>
+          Волкова, 5А, Нижний Новгород, 603041, Россия
+        </p>
+
+        <div className={styles.maps}>
+          <YMaps>
+            <Map className={styles.ymap} width={'100%'} defaultState={{ center: [56.24271361610231, 43.83043802718367], zoom: 17, width: 100 }}>
+              <Placemark defaultGeometry={[56.24271361610231, 43.83043802718367]} />
+            </Map>
+          </YMaps>
+        </div>
+
+        <h2>
+          Доставка
+        </h2>
+        <p>
+          Мы доставим ваш заказ в любой город России. Доставка осуществляется транспортными компаниями.
+          <br />
+          Доставка по Нижнему Новгороду: 500 рублей. При заказе свыше 30 000 рублей доставка бесплатная.
+        </p>
+        <h2>Реквизиты</h2>
+        <p>
+          ООО «Поволжье-спецодежда»
+          <br />
+          ИНН: 5260122994, КПП: 503201001, ОГРН: 1035205418181
+          <br />
+          Юр. Адрес: 143005, Московская обл., Одинцовский р-н, г. Одинцово, ул. Вокзальная д. 19, кв. 17
         </p>
       </div>
     </div>
